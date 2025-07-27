1a3c67f38b380a65d832092ef6f84e0f07a448d9
Thiago Borges Vieira <tborgesvieira@gmail.com>
2025-07-27 13:59:55 -0300

    Projeto de estudo

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return (
+        <div className={
+            `
+                bg-purple-500                 
+                p-3
+                text-xl
+            `
+        }>
+            Header
+        </div>
+    )    
+}
